882ef911c2e6382f2575730c23a006ce8cb6e3ca
psychofield <yacine.cheikhrouhou@yahoo.fr>
2015-12-19 17:57:13 +0100

    ajouts sur diagramme

--- src/perso/Stats.java
+++ src/perso/Stats.java
@@ -5,7 +5,8 @@ public class Stats {
 	//str for strength, dex for dexterity, vit for vitality,
 	//intel for intelligence, wis for wisdom, luk for luck
 	int str, dex, vit, intel, wis, luk;
-	
+	//c for current, this is the value that can be changed by status
+	int cstr, cdex, cvit, cintel, cwis, cluk;
 
 	public Stats() {
 		//TODO add default constructor
@@ -16,6 +17,7 @@ public class Stats {
 		intel = 10;
 		wis = 10;
 		luk = 10;
+		this.reloadTempStats();
 	}
 	
 	public Stats(int str, int dex, int vit, int intel, int wis, int luk){
@@ -25,9 +27,17 @@ public class Stats {
 		this.intel = intel;
 		this.wis = wis;
 		this.luk = luk;
+		this.reloadTempStats();
 	}
 	
-	
+	public void reloadTempStats(){
+		cstr = str;
+		cdex = dex;
+		cvit = vit;
+		cintel = intel;
+		cwis = wis;
+		cluk = luk;
+	}
 	//getters
 	public int getStr() {
 		return str;
@@ -46,6 +56,10 @@ public class Stats {
 	}
 	public int getLuk() {
 		return luk;
+	}
+	//setters
+	public void tempSetStr(int value){
+		this.cstr = value;
 	}
 
 }
