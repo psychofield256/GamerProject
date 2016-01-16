63c01315996d3b819c3bf601203daa300d890fb5
psychofield <yacine.cheikhrouhou@yahoo.fr>
2016-01-16 09:20:28 +0100

    testing some drawing functions

--- tests/guiOOP.py
+++ tests/guiOOP.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#made while learning with thepythongamebook.com/en:pygame:step003
 
 import pygame
 
@@ -12,10 +12,11 @@ class PygView():
 		self.height = height
 		self.screen = pygame.display.set_mode((self.width, self.height), pygame.DOUBLEBUF)
 		self.background = pygame.Surface(self.screen.get_size()).convert() #convert for faster rendering
+		self.background.fill((255,255,255))
 		self.clock = pygame.time.Clock()
 		self.fps = fps
-		self.playtime = 0.0
 		self.font = pygame.font.SysFont('mono', 20, bold=True)
+		
 
 	def run(self):
 		"""Mainloop"""
@@ -28,13 +29,14 @@ class PygView():
 					if event.key == pygame.K_ESCAPE:
 						running = False
 
-			miliseconds = self.clock.tick(self.fps)
-			self.playtime += miliseconds / 1000.0
-			self.draw_text("FPS : {0:.3f} PLAYTIME : {0:.3f} SECONDS".format(
-				self.clock.get_fps(), self.playtime))
+			self.clock.tick(self.fps)
 
-			pygame.display.flip()
+			
+			#pygame.draw.circle(self.background, (255,192,203), (self.screen.get_size()[0]//2, self.screen.get_size()[1]//2), 200)
 			self.screen.blit(self.background, (0,0))
+
+			pygame.draw.arc(self.background, (0,150,0),(400,10,150,100), 0, 3.14) 
+			pygame.display.flip()
 
 		pygame.quit()
 
@@ -42,8 +44,11 @@ class PygView():
 		"""center text"""
 		fw, fh = self.font.size(text) #fw: fontweight, fh: fontheight
 		surface = self.font.render(text, True, (0, 255, 0))
+		#print in the middle of the screen
 		self.screen.blit(surface, ((self.width - fw) // 2, (self.height - fh) // 2))
 
+	def set_title(self, text):
+		pygame.display.set_caption(text)
 
 if __name__ == "__main__":
 	PygView().run()
