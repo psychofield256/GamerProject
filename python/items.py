109cb2a8d25469af9e3071b166de261d9f0533d3
psychofield <yacine.cheikhrouhou@yahoo.fr>
2016-01-22 22:14:09 +0100

    Clean items.py, created todo.txt/done.txt

--- python/items.py
+++ python/items.py
@@ -1,87 +1,87 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-#IMPORTANT:
+"""
+This file contains all the informations about items.
 
+Ordinary items are in the items list, jewels are in the jewels list, and
+equipments are in the equipments list
+"""
 
-#items
-items = [
-	#example
-	{
-		"name": "item name",
-		"lore": "description",
-		"weight": 1,
-	},
-	
+ITEMS = [
+    # example
+    {
+        "name": "item name",
+        "lore": "description",
+        "weight": 1,
+    },
+
 ]
 
-jewels = [
-	#jewel example
-	{
-		"name": "jewel test",
-		"lore": "A jewel that can decorate an equipment",
-		"weight": 1,
-		"str": 300, "dex": 300, "vit": 300,
-		"int": 300, "wis": 300, "luk": 300 #no, that's not cheat
-	}
+JEWELS = [
+    # jewel example
+    {
+        "name": "jewel test",
+        "lore": "A jewel that can decorate an equipment",
+        "weight": 1,
+        "str": 300, "dex": 300, "vit": 300,
+        "int": 300, "wis": 300, "luk": 300
+    }
 ]
 
-#equipments
-equipments = [
-	#example
-	{
-		"name": "Example Sword",
-		"lore": "description",
-		"lvl": 1,
-		"weight": 10,
-		"emptySlots": 1, #emptySlots if for inserting items which provide stat boosts
-		"slot": "weapon", #weapon, helmet, chestplate, leggings or boots
-		"type": "sword", #sword, axe, bow...
-		"str": 100, "dex": 100, "vit": 100,
-		"int": 100, "wis": 100, "luk": 100 #no, that's not cheat. that's programming.
-	}, {
-		"name": "sword of devil",
-		"lore": "the sword left by the blue demon",
-		"lvl": 10,
-		"weight": 10,
-		"emptySlots": 2, 
-		"slot": "weapon",
-		"type": "sword",
-		"str": 10, "dex": 15, "vit": 10,
-		"int": 0, "wis": 0, "luk": 0
-	}, {
-		"name": "wooden stick",
-		"lore": "a wooden stick you \"borrowed\" at someone you didn't know",
-		"lvl": 1,
-		"weight": 12,
-		"insertable": False,
-		"emptySlots": 0,
-		"slot": "weapon",
-		"type": "sword",
-		"str": 5, "dex": 0, "vit": 0,
-		"int": 0, "wis": 0, "luk": 0
-	}, {
-		"name": "test chestplate",
-		"lore": "a wonderful cheat armor",
-		"lvl": 1,
-		"weight": 0,
-		"emptySlots": 5,
-		"slot": "chestplate",
-		"type": "LightArmor",
-		"str": 500, "dex": 500, "vit": 500,
-		"int": 500, "wis": 500, "luk": 500
-	}, {
-		"name": "helmet",
-		"lore": "cheat test helmet",
-		"lvl": 1,
-		"weight": 0,
-		"emptySlots": 4,
-		"slot": "helmet",
-		"type": "LightArmor",
-		"str": 500, "dex": 500, "vit": 500,
-		"int": 500, "wis": 500, "luk": 500
-	}
+EQUIPMENTS = [
+    # example
+    {
+        "name": "Example Sword",
+        "lore": "description",
+        "lvl": 1,
+        "weight": 10,
+        "emptySlots": 1,
+        # emptySlots if for inserting items, which provide stat boosts
+        "slot": "weapon",  # weapon, helmet, chestplate, leggings or boots
+        "type": "sword",  # sword, axe, bow...
+        "str": 100, "dex": 100, "vit": 100,
+        "int": 100, "wis": 100, "luk": 100
+    }, {
+        "name": "sword of devil",
+        "lore": "the sword left by the blue demon",
+        "lvl": 10,
+        "weight": 10,
+        "emptySlots": 2,
+        "slot": "weapon",
+        "type": "sword",
+        "str": 10, "dex": 15, "vit": 10,
+        "int": 0, "wis": 0, "luk": 0
+    }, {
+        "name": "wooden stick",
+        "lore": "a wooden stick you \"borrowed\" at someone you didn't know",
+        "lvl": 1,
+        "weight": 12,
+        "insertable": False,
+        "emptySlots": 0,
+        "slot": "weapon",
+        "type": "sword",
+        "str": 5, "dex": 0, "vit": 0,
+        "int": 0, "wis": 0, "luk": 0
+    }, {
+        "name": "test chestplate",
+        "lore": "a wonderful cheat armor",
+        "lvl": 1,
+        "weight": 0,
+        "emptySlots": 5,
+        "slot": "chestplate",
+        "type": "LightArmor",
+        "str": 500, "dex": 500, "vit": 500,
+        "int": 500, "wis": 500, "luk": 500
+    }, {
+        "name": "helmet",
+        "lore": "cheat test helmet",
+        "lvl": 1,
+        "weight": 0,
+        "emptySlots": 4,
+        "slot": "helmet",
+        "type": "LightArmor",
+        "str": 500, "dex": 500, "vit": 500,
+        "int": 500, "wis": 500, "luk": 500
+    }
 ]
-
-
-
