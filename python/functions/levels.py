545867fdcd2dedfbfbc6ad76e72516ac88b6e127
psychofield <yacine.cheikhrouhou@yahoo.fr>
2016-02-09 08:52:49 +0100

    add an empty line in the doc

--- python/functions/levels.py
+++ python/functions/levels.py
@@ -10,6 +10,7 @@ def getexp(lvl):
     The condition to level up is: exp = (lvl+1)^3,
     where exp is the experience needed, and lvl+1
     the next level (lvl is the current level)
+    
     To remove the possibility of having the level 0,
     change "lvl > 0" into "lvl > 1" (however, tests won't work as
     I made them for the level 0)
@@ -30,6 +31,7 @@ def getlvl(exp):
     lvl is the current level.
     Here, we are calculating each time the exp needed to
     up with the next level (lvl+1).
+
     To remove the possibility of having the level 0
     (which I didn't test because I don't want to rewrite this
     another time), change all the "(lvl+1)" into "lvl" (however,
