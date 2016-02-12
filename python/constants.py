60cd9f9c56fa0ec938518f96f580bdc79ea8f62a
psychofield256 <yacine.cheikhrouhou@yahoo.fr>
2016-02-12 23:18:44 +0100

    add constant object, easy to use and more pretty than capitalized names

--- python/constants.py
+++ python/constants.py
@@ -1,5 +1,9 @@
 """
-Constants used in the project
+Constants used in the project.
+
+Imported with import constants (as c)
+The constants can be set at every moment, because
+import will return an instance, not the module.
 """
 
 STATLIST = ["str", "dex", "vit", "int", "wis", "luk"]
@@ -14,3 +18,29 @@ emptystats = {
 	"str": 0, "dex": 0, "vit": 0,
 	"int": 0, "wis": 0, "luk": 0,
 }
+
+class Constants(object):
+    """Constants of the project."""
+
+    class ConstantError(TypeError): pass
+
+    # def __init__(self):
+    # """Create the constants."""
+    info_list = ["name", "lvl", "lore", "weight"]
+
+    def __setattr__(self, attr, value):
+        """Block redefining a constant."""
+        # if there is already a constant, raise
+        if hasattr(self, attr):
+            raise self.ConstantError("A constant cannot be changed.")
+        # else, create it
+        object.__setattr__(self, attr, value)
+
+
+
+# this allows to import the module and get the instance with
+# basic constants in it. Other can be added
+c = Constants()
+
+import sys
+sys.modules[__name__] = c
