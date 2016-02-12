c8949280c310818863abf3e8f7cd247e41d5eef7
psychofield256 <yacine.cheikhrouhou@yahoo.fr>
2016-02-12 23:19:16 +0100

    test Bunch.__str__

--- python/main2.py
+++ python/main2.py
@@ -7,11 +7,21 @@ Its objectives are to adapt the manga "The Gamer" as a real rpg.
 """
 
 import json
+from bunch import Bunch
+
+import constants as c
+from functions.display import new_bunch_str as n
+
+# redefine Bunch.__str__()
+Bunch.__str__ = n
+
+weapon_shop = []
 
 with open("config/town/weapon_shop.json", "r") as f:
-	weapons = json.load(f)
+	for weapon in json.load(f):
+		weapon_shop.append(Bunch(**weapon))
 
-for weapon in weapons:
+
+for weapon in weapon_shop:
 	print("---------------")
-	print(weapon["name"] + ":")
 	print(weapon)
