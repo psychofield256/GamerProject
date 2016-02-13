18dad69d79e22d5c7b255f4fb93d89074910b74b
psychofield256 <yacine.cheikhrouhou@yahoo.fr>
2016-02-13 12:13:02 +0100

    add space indent in parser.py

--- python/parser.py
+++ python/parser.py
@@ -8,7 +8,7 @@ It contains the bunches for items.
 import json
 from bunch import Bunch
 
-import constants as c
+# import constants as c
 from functions.display import new_bunch_str as n
 
 # redefines Bunch.__str__ (in order not to create a new class for this)
@@ -18,12 +18,23 @@ item_shop = []
 weapon_shop = []
 armor_shop = []
 
-with open("config/town/items.json", "r") as f:
-	for item in json.load(f):
-		# unpack the dict to make a Bunch
-		item = Bunch(**item)
-		# tag it ro recognize it after
-		item.type = "item"
-		# add it to the shop
-		item_shop.append(item)
+with open("config/town/item_shop.json", "r") as f:
+    for item in json.load(f):
+        # unpack the dict to make a Bunch
+        item = Bunch(**item)
+        # tag it ro recognize it after
+        item.type = "item"
+        # add it to the shop
+        item_shop.append(item)
 
+with open("config/town/weapon_shop.json", "r") as f:
+    for weapon in json.load(f):
+        weapon = Bunch(**weapon)
+        weapon.type = "equipment"
+        weapon_shop.append(weapon)
+
+with open("config/town/armor_shop.json", "r") as f:
+    for equip in json.load(f):
+        equip = Bunch(**equip)
+        equip.type = "equipment"
+        armor_shop.append(equip)
