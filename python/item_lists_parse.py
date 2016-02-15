ffcc3b44f74963158ffd4097e876b06714fd59e3
psychofield256 <yacine.cheikhrouhou@yahoo.fr>
2016-02-15 10:25:39 +0100

    implement the code of the json parser

--- python/item_lists_parse.py
+++ python/item_lists_parse.py
@@ -13,3 +13,34 @@ import sys
 items = []
 weapons = []
 armors = []
+
+# parse the json files
+with open("config/items/item_list.json", "r") as f:
+    for item in json.load(f):
+        # tag it ro recognize it after
+        item["type"] = "item"
+        # add it to the shop
+        items.append(item)
+
+with open("config/items/weapon_list.json", "r") as f:
+    for weapon in json.load(f):
+        weapon["type"] = "equipment"
+        # this is not defined before for a better looking json
+        weapon["slot"] = "weapon"
+        # already inserted items
+        weapon["gems"] = []
+        weapons.append(weapon)
+
+with open("config/items/armor_list.json", "r") as f:
+    for equip in json.load(f):
+        equip["type"] = "equipment"
+        equip["gems"] = []
+        armors.append(equip)
+
+shops = {
+    "items": items,
+    "weapons": weapons,
+    "armors": armors
+}
+
+sys.modules[__name__] = shops
