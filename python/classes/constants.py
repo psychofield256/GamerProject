a2b7e87aaf6e66a6cbe2c19acdc7573b07c27c25
psychofield256 <yacine.cheikhrouhou@yahoo.fr>
2016-02-16 22:29:31 +0100

    correctly implemented classes/constants

--- python/classes/constants.py
+++ python/classes/constants.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
+# pylint: disable=wrong-import-position,import-self
 """
 Import constants from upper directory and replace itself with them.
 
 It is used for not replacing the same ugly code in every module
 that need a constant.
+
+Pylint:
+wrong-import-position is disabled because I can't import
+something before changing pythonpath to specify its location.
+import-self is disabled because as the main directory is
+at index 0, it will be used before this file.
 """
 
 import os
 import sys
 
 # if pythonpath is not main.py, go up until you're at it and register it
-directory = os.path.abspath(__file__)
-while not directory.endswith("python"):
-    directory = os.path.dirname(directory)
-sys.path.insert(0, directory)
+MAIN_DIR = os.path.abspath(__file__)
 
+while not MAIN_DIR.endswith("python"):
+    MAIN_DIR = os.path.dirname(MAIN_DIR)
 
+sys.path.insert(0, MAIN_DIR)
 import constants
 
 sys.modules[__name__] = constants
